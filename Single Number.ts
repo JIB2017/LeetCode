d4510a26e8d1fad99c0116e7004cfb19c54f5da0
JIB2017 <blacuttjuan07@gmail.com>
2024-05-10 00:11:45 -0300

    Single Number HashMap solution & complexity evaluation

--- Single Number.ts
+++ Single Number.ts
@@ -1,3 +1,6 @@
+// XOR solution
+// Time complexity: O(n)
+// Spacial complexity: O(1)
 function singleNumber(nums: number[]): number {
   let aux = 0;
 
@@ -6,4 +9,26 @@ function singleNumber(nums: number[]): number {
   }
 
   return aux;
+}
+
+// HashMap solution
+// Time complexity: O(n)
+// Spacial complexity: O(n)
+function singleNumber2(array: number[]): number | null {
+  let map = new Map();
+
+  for (let nro of array) {
+    if (map.has(nro)) {
+      map.set(nro, map.get(nro) + 1);
+    } else {
+      map.set(nro, 1);
+    }
+  }
+
+  for (let [key, value] of map.entries()) {
+    if (value === 1) {
+      return key;
+    }
+  }
+  return null;
 }
